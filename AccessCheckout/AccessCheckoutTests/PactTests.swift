7a811881f580dfef1a8984c3e19f97698011f17b
Matt Davison <mattdavison@pixemotive.com>
2019-06-04 15:35:07 +0100

    NEB-452: Pact error tests

--- AccessCheckout/AccessCheckoutTests/PactTests.swift
+++ AccessCheckout/AccessCheckoutTests/PactTests.swift
@@ -49,4 +49,128 @@ class PactTests: XCTestCase {
             }
         }
     }
+    
+    func testError_variation1() {
+        
+        let stub = StubProvider.stub(forName: "VerifiedTokens-unknown-variation1",
+                                     bundle: Bundle(for: type(of: self)))
+        
+        guard let data = stub?.data, let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else {
+            XCTFail()
+            return
+        }
+        
+        verifiedTokensMockService
+            .given("the service returns an error; variation 1")
+            .uponReceiving("a request to create a session")
+            .withRequest(method: .POST, path: "/verifiedTokens/sessions")
+            .willRespondWith(status: 500,
+                             headers: ["Content-Type": "application/json; charset=utf-8"],
+                             body: json)
+        
+        let mockDiscovery = MockDiscovery(baseURI: verifiedTokensMockService.baseUrl)
+        let verifiedTokensClient = AccessCheckoutClient(discovery: mockDiscovery, merchantIdentifier: "identity")
+        verifiedTokensMockService.run(timeout: 10) { testComplete in
+            verifiedTokensClient.createSession(pan: "",
+                                               expiryMonth: 0,
+                                               expiryYear: 0,
+                                               cvv: "",
+                                               urlSession: URLSession.shared) { result in
+                                                testComplete()
+            }
+        }
+    }
+    
+    func testError_variation2() {
+        
+        let stub = StubProvider.stub(forName: "VerifiedTokens-unknown-variation2",
+                                     bundle: Bundle(for: type(of: self)))
+        
+        guard let data = stub?.data, let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else {
+            XCTFail()
+            return
+        }
+        
+        verifiedTokensMockService
+            .given("the service returns an error; variation 2")
+            .uponReceiving("a request to create a session")
+            .withRequest(method: .POST, path: "/verifiedTokens/sessions")
+            .willRespondWith(status: 500,
+                             headers: ["Content-Type": "application/json; charset=utf-8"],
+                             body: json)
+        
+        let mockDiscovery = MockDiscovery(baseURI: verifiedTokensMockService.baseUrl)
+        let verifiedTokensClient = AccessCheckoutClient(discovery: mockDiscovery, merchantIdentifier: "identity")
+        verifiedTokensMockService.run(timeout: 10) { testComplete in
+            verifiedTokensClient.createSession(pan: "",
+                                               expiryMonth: 0,
+                                               expiryYear: 0,
+                                               cvv: "",
+                                               urlSession: URLSession.shared) { result in
+                                                testComplete()
+            }
+        }
+    }
+    
+    func testError_variation3() {
+        
+        let stub = StubProvider.stub(forName: "VerifiedTokens-unknown-variation3",
+                                     bundle: Bundle(for: type(of: self)))
+        
+        guard let data = stub?.data, let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else {
+            XCTFail()
+            return
+        }
+        
+        verifiedTokensMockService
+            .given("the service returns an error; variation 3")
+            .uponReceiving("a request to create a session")
+            .withRequest(method: .POST, path: "/verifiedTokens/sessions")
+            .willRespondWith(status: 500,
+                             headers: ["Content-Type": "application/json; charset=utf-8"],
+                             body: json)
+        
+        let mockDiscovery = MockDiscovery(baseURI: verifiedTokensMockService.baseUrl)
+        let verifiedTokensClient = AccessCheckoutClient(discovery: mockDiscovery, merchantIdentifier: "identity")
+        verifiedTokensMockService.run(timeout: 10) { testComplete in
+            verifiedTokensClient.createSession(pan: "",
+                                               expiryMonth: 0,
+                                               expiryYear: 0,
+                                               cvv: "",
+                                               urlSession: URLSession.shared) { result in
+                                                testComplete()
+            }
+        }
+    }
+    
+    func testError_variation4() {
+        
+        let stub = StubProvider.stub(forName: "VerifiedTokens-unknown-variation4",
+                                     bundle: Bundle(for: type(of: self)))
+        
+        guard let data = stub?.data, let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else {
+            XCTFail()
+            return
+        }
+        
+        verifiedTokensMockService
+            .given("the service returns an error; variation 4")
+            .uponReceiving("a request to create a session")
+            .withRequest(method: .POST, path: "/verifiedTokens/sessions")
+            .willRespondWith(status: 500,
+                             headers: ["Content-Type": "application/json; charset=utf-8"],
+                             body: json)
+        
+        let mockDiscovery = MockDiscovery(baseURI: verifiedTokensMockService.baseUrl)
+        let verifiedTokensClient = AccessCheckoutClient(discovery: mockDiscovery, merchantIdentifier: "identity")
+        verifiedTokensMockService.run(timeout: 10) { testComplete in
+            verifiedTokensClient.createSession(pan: "",
+                                               expiryMonth: 0,
+                                               expiryYear: 0,
+                                               cvv: "",
+                                               urlSession: URLSession.shared) { result in
+                                                testComplete()
+            }
+        }
+    }
 }
