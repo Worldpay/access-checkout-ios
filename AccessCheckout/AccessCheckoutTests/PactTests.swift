0e1edd791188152300dea9a60cd3356eb097c3fa
David Vilares Ferrer <david.vilaresferrer@worldpay.com>
2019-06-17 10:57:56 +0100

    NEB-452: Refactor first two tests

--- AccessCheckout/AccessCheckoutTests/PactTests.swift
+++ AccessCheckout/AccessCheckoutTests/PactTests.swift
@@ -23,8 +23,18 @@ class PactTests: XCTestCase {
 
     func testCreateSession() {
         
+        let requestJson : [String: Any] = [
+            "cvc": "123",
+            "identity": "identity",
+            "cardNumber": "4111111111111111",
+            "cardExpiryDate": [
+                "month": 12,
+                "year": 2099
+            ]
+        ]
+
         let expectedValue = "\(baseURI)/verifiedTokens/sessions"
-        let json = [
+        let responseJson = [
             "_links": [
                 "verifiedTokens:session": [
                     "href": Matcher.term(matcher: "https?://[^/]+/verifiedTokens/sessions", generate: expectedValue)
@@ -35,10 +45,10 @@ class PactTests: XCTestCase {
         verifiedTokensMockService
             .given("a session is available")
             .uponReceiving("a request to create a session")
-            .withRequest(method: .POST, path: "/verifiedTokens/sessions")
+            .withRequest(method: .POST, path: "/verifiedTokens/sessions", body: requestJson)
             .willRespondWith(status: 201,
-                             headers: ["Content-Type": "application/json; charset=utf-8"],
-                             body: json)
+                             headers: ["Content-Type": "application/vnd.worldpay.verified-tokens-v1.hal+json;charset=UTF-8"],
+                             body: responseJson)
         
         let mockDiscovery = MockDiscovery(baseURI: verifiedTokensMockService.baseUrl)
         let verifiedTokensClient = AccessCheckoutClient(discovery: mockDiscovery, merchantIdentifier: "identity")
@@ -60,126 +70,57 @@ class PactTests: XCTestCase {
         }
     }
     
-    func testError_variation1() {
+    func testProvideIncorrectMerchantId() {
         
-        let stub = StubProvider.stub(forName: "VerifiedTokens-unknown-variation1",
-                                     bundle: Bundle(for: type(of: self)))
+        let requestJson : [String: Any] = [
+            "cvc": "123",
+            "identity": "incorrectValue",
+            "cardNumber": "4111111111111111",
+            "cardExpiryDate": [
+                "month": 12,
+                "year": 2099
+            ]
+        ]
         
-        guard let data = stub?.data, let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else {
-            XCTFail()
-            return
-        }
-        
+        let responseJson : [String : Any] = [
+            "errorName": "bodyDoesNotMatchSchema",
+            "validationErrors": [
+                [
+                "errorName": "fieldHasInvalidValue",
+                "jsonPath": "$.identity"
+                ]
+            ]
+        ]
+
         verifiedTokensMockService
             .given("the service returns an error; variation 1")
             .uponReceiving("a request to create a session")
-            .withRequest(method: .POST, path: "/verifiedTokens/sessions")
-            .willRespondWith(status: 500,
-                             headers: ["Content-Type": "application/json; charset=utf-8"],
-                             body: json)
+            .withRequest(method: .POST,
+                         path: "/verifiedTokens/sessions",
+                         headers: [:],
+                         body: requestJson)
+            .willRespondWith(status: 400,
+                             headers: ["Content-Type": "application/vnd.worldpay.verified-tokens-v1.hal+json;charset=UTF-8"],
+                             body: responseJson)
         
         let mockDiscovery = MockDiscovery(baseURI: verifiedTokensMockService.baseUrl)
-        let verifiedTokensClient = AccessCheckoutClient(discovery: mockDiscovery, merchantIdentifier: "identity")
+        let verifiedTokensClient = AccessCheckoutClient(discovery: mockDiscovery, merchantIdentifier: "incorrectValue")
+        
         verifiedTokensMockService.run(timeout: 10) { testComplete in
-            verifiedTokensClient.createSession(pan: "",
-                                               expiryMonth: 0,
-                                               expiryYear: 0,
-                                               cvv: "",
+            verifiedTokensClient.createSession(pan: "4111111111111111",
+                                               expiryMonth: 12,
+                                               expiryYear: 2099,
+                                               cvv: "123",
                                                urlSession: URLSession.shared) { result in
-                                                testComplete()
-            }
-        }
-    }
-    
-    func testError_variation2() {
-        
-        let stub = StubProvider.stub(forName: "VerifiedTokens-unknown-variation2",
-                                     bundle: Bundle(for: type(of: self)))
-        
-        guard let data = stub?.data, let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else {
-            XCTFail()
-            return
-        }
-        
-        verifiedTokensMockService
-            .given("the service returns an error; variation 2")
-            .uponReceiving("a request to create a session")
-            .withRequest(method: .POST, path: "/verifiedTokens/sessions")
-            .willRespondWith(status: 500,
-                             headers: ["Content-Type": "application/json; charset=utf-8"],
-                             body: json)
-        
-        let mockDiscovery = MockDiscovery(baseURI: verifiedTokensMockService.baseUrl)
-        let verifiedTokensClient = AccessCheckoutClient(discovery: mockDiscovery, merchantIdentifier: "identity")
-        verifiedTokensMockService.run(timeout: 10) { testComplete in
-            verifiedTokensClient.createSession(pan: "",
-                                               expiryMonth: 0,
-                                               expiryYear: 0,
-                                               cvv: "",
-                                               urlSession: URLSession.shared) { result in
-                                                testComplete()
-            }
-        }
-    }
-    
-    func testError_variation3() {
-        
-        let stub = StubProvider.stub(forName: "VerifiedTokens-unknown-variation3",
-                                     bundle: Bundle(for: type(of: self)))
-        
-        guard let data = stub?.data, let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else {
-            XCTFail()
-            return
-        }
-        
-        verifiedTokensMockService
-            .given("the service returns an error; variation 3")
-            .uponReceiving("a request to create a session")
-            .withRequest(method: .POST, path: "/verifiedTokens/sessions")
-            .willRespondWith(status: 500,
-                             headers: ["Content-Type": "application/json; charset=utf-8"],
-                             body: json)
-        
-        let mockDiscovery = MockDiscovery(baseURI: verifiedTokensMockService.baseUrl)
-        let verifiedTokensClient = AccessCheckoutClient(discovery: mockDiscovery, merchantIdentifier: "identity")
-        verifiedTokensMockService.run(timeout: 10) { testComplete in
-            verifiedTokensClient.createSession(pan: "",
-                                               expiryMonth: 0,
-                                               expiryYear: 0,
-                                               cvv: "",
-                                               urlSession: URLSession.shared) { result in
-                                                testComplete()
-            }
-        }
-    }
-    
-    func testError_variation4() {
-        
-        let stub = StubProvider.stub(forName: "VerifiedTokens-unknown-variation4",
-                                     bundle: Bundle(for: type(of: self)))
-        
-        guard let data = stub?.data, let json = try? JSONSerialization.jsonObject(with: data, options: .allowFragments) else {
-            XCTFail()
-            return
-        }
-        
-        verifiedTokensMockService
-            .given("the service returns an error; variation 4")
-            .uponReceiving("a request to create a session")
-            .withRequest(method: .POST, path: "/verifiedTokens/sessions")
-            .willRespondWith(status: 500,
-                             headers: ["Content-Type": "application/json; charset=utf-8"],
-                             body: json)
-        
-        let mockDiscovery = MockDiscovery(baseURI: verifiedTokensMockService.baseUrl)
-        let verifiedTokensClient = AccessCheckoutClient(discovery: mockDiscovery, merchantIdentifier: "identity")
-        verifiedTokensMockService.run(timeout: 10) { testComplete in
-            verifiedTokensClient.createSession(pan: "",
-                                               expiryMonth: 0,
-                                               expiryYear: 0,
-                                               cvv: "",
-                                               urlSession: URLSession.shared) { result in
-                                                testComplete()
+                switch result {
+                case .success(_):
+                    XCTFail("Service response expected to be unsuccessful")
+                case let .failure(error):
+                    XCTAssertTrue(error.localizedDescription.contains("bodyDoesNotMatchSchema"), "Error msg must contain general error code")
+                    XCTAssertTrue(error.localizedDescription.contains("fieldHasInvalidValue"), "Error msg must contain specific validation error code")
+                    XCTAssertTrue(error.localizedDescription.contains("$.identity"), "Error msg must contain path to error value")
+                }
+                testComplete()
             }
         }
     }
