bc3991663cca46bb697db83ce5806aa19ee6daf8
David Vilares Ferrer <david.vilaresferrer@worldpay.com>
2019-06-20 17:48:31 +0100

    NEB-452: DRY: Abstract negative test cases

--- AccessCheckout/AccessCheckoutTests/PactTests.swift
+++ AccessCheckout/AccessCheckoutTests/PactTests.swift
@@ -6,6 +6,9 @@ class PactTests: XCTestCase {
 
     let baseURI: String = Bundle(for: PactTests.self).infoDictionary?["ACCESS_CHECKOUT_BASE_URI"] as? String ?? "https://access.worldpay.com"
     
+    let requestHeaders: [String: Any] = ["content-type": "application/vnd.worldpay.verified-tokens-v1.hal+json"]
+    let responseHeaders: [String: Any] = ["Content-Type": "application/vnd.worldpay.verified-tokens-v1.hal+json;charset=UTF-8"]
+
     let verifiedTokensMockService = MockService(provider: "verified-tokens",
                                                 consumer: "access-checkout-iOS-sdk")
     
@@ -43,11 +46,14 @@ class PactTests: XCTestCase {
         ]
         
         verifiedTokensMockService
-            .given("verified tokens is running")
             .uponReceiving("a valid request")
-            .withRequest(method: .POST, path: "/verifiedTokens/sessions", body: requestJson)
+            .withRequest(
+                method: .POST,
+                path: "/verifiedTokens/sessions",
+                headers: requestHeaders,
+                body: requestJson)
             .willRespondWith(status: 201,
-                             headers: ["Content-Type": "application/vnd.worldpay.verified-tokens-v1.hal+json;charset=UTF-8"],
+                             headers: responseHeaders,
                              body: responseJson)
         
         let mockDiscovery = MockDiscovery(baseURI: verifiedTokensMockService.baseUrl)
@@ -72,114 +78,114 @@ class PactTests: XCTestCase {
     
     func testProvideIncorrectMerchantId() {
         
-        let requestJson : [String: Any] = [
-            "cvc": "123",
-            "identity": "incorrectValue",
-            "cardNumber": "4111111111111111",
-            "cardExpiryDate": [
-                "month": 12,
-                "year": 2099
-            ]
-        ]
+        let request = PactRequest(
+                identity: "incorrectValue",
+                cvc: "123",
+                cardNumber: "4111111111111111",
+                expiryMonth: 12,
+                expiryYear: 2099)
         
-        let responseJson : [String : Any] = [
-            "errorName": "bodyDoesNotMatchSchema",
-            "message": Matcher.somethingLike("The json body provided does not match the expected schema"),
-            "validationErrors": [
-                [
-                "errorName": "fieldHasInvalidValue",
-                "message": Matcher.somethingLike("Identity is invalid"),
-                "jsonPath": "$.identity"
-                ]
-            ]
-        ]
-
-        verifiedTokensMockService
-            .given("verified tokens is running")
-            .uponReceiving("a request with an invalid identity")
-            .withRequest(method: .POST,
-                         path: "/verifiedTokens/sessions",
-                         headers: [:],
-                         body: requestJson)
-            .willRespondWith(status: 400,
-                             headers: ["Content-Type": "application/vnd.worldpay.verified-tokens-v1.hal+json;charset=UTF-8"],
-                             body: responseJson)
+        let expectedErrorResponse = ExpectedPactErrorResponse(
+                mainErrorName: "bodyDoesNotMatchSchema",
+                mainErrorMessage: "The json body provided does not match the expected schema",
+                validationErrorName: "fieldHasInvalidValue",
+                validationErrorMessage: "Identity is invalid",
+                validationJsonPath: "$.identity")
         
-        let mockDiscovery = MockDiscovery(baseURI: verifiedTokensMockService.baseUrl)
-        let verifiedTokensClient = AccessCheckoutClient(discovery: mockDiscovery, merchantIdentifier: "incorrectValue")
-        
-        verifiedTokensMockService.run(timeout: 10) { testComplete in
-            verifiedTokensClient.createSession(pan: "4111111111111111",
-                                               expiryMonth: 12,
-                                               expiryYear: 2099,
-                                               cvv: "123",
-                                               urlSession: URLSession.shared) { result in
-                switch result {
-                case .success(_):
-                    XCTFail("Service response expected to be unsuccessful")
-                case let .failure(error):
-                    XCTAssertTrue(error.localizedDescription.contains("bodyDoesNotMatchSchema"), "Error msg must contain general error code")
-                    XCTAssertTrue(error.localizedDescription.contains("fieldHasInvalidValue"), "Error msg must contain specific validation error code")
-                    XCTAssertTrue(error.localizedDescription.contains("$.identity"), "Error msg must contain path to error value")
-                }
-                testComplete()
-            }
-        }
+        performTestCase(forScenario: "a request with an invalid identity", withRequest: request, andErrorResponse: expectedErrorResponse)
     }
     
     func testPANDoesNotPassLuhnCheck() {
         
+        let request = PactRequest(
+            identity: "identity",
+            cvc: "123",
+            cardNumber: "4111111111111110",
+            expiryMonth: 12,
+            expiryYear: 2099)
+        
+        let expectedErrorResponse = ExpectedPactErrorResponse(
+            mainErrorName: "bodyDoesNotMatchSchema",
+            mainErrorMessage: "The json body provided does not match the expected schema",
+            validationErrorName: "panFailedLuhnCheck",
+            validationErrorMessage: "The identified field contains a PAN that has failed the Luhn check.",
+            validationJsonPath: "$.cardNumber")
+        
+        performTestCase(forScenario: "a request with a PAN that does not pass Luhn check", withRequest: request, andErrorResponse: expectedErrorResponse)
+    }
+
+
+    private struct PactRequest {
+        let identity: String
+        let cvc: String
+        let cardNumber: String
+        let expiryMonth: UInt
+        let expiryYear: UInt
+    }
+    
+    private struct ExpectedPactErrorResponse {
+        let mainErrorName: String
+        let mainErrorMessage: String
+        let validationErrorName: String
+        let validationErrorMessage: String
+        let validationJsonPath: String
+    }
+    
+    private func performTestCase(
+                    forScenario scenario: String,
+                    withRequest request: PactRequest,
+                    andErrorResponse response: ExpectedPactErrorResponse) {
+        
         let requestJson : [String: Any] = [
-            "cvc": "123",
-            "identity": "identity",
-            "cardNumber": "4111111111111110",
+            "cvc": request.cvc,
+            "identity": request.identity,
+            "cardNumber": request.cardNumber,
             "cardExpiryDate": [
-                "month": 12,
-                "year": 2099
+                "month": request.expiryMonth,
+                "year": request.expiryYear
             ]
         ]
         
         let responseJson : [String : Any] = [
-            "errorName": "bodyDoesNotMatchSchema",
-            "message": Matcher.somethingLike("The json body provided does not match the expected schema"),
+            "errorName": response.mainErrorName,
+            "message": Matcher.somethingLike(response.mainErrorMessage),
             "validationErrors": [
                 [
-                    "errorName": "panFailedLuhnCheck",
-                    "message": Matcher.somethingLike("The identified field contains a PAN that has failed the Luhn check."),
-                    "jsonPath": "$.cardNumber"
+                    "errorName": response.validationErrorName,
+                    "message": Matcher.somethingLike(response.validationErrorMessage),
+                    "jsonPath": response.validationJsonPath
                 ]
             ]
         ]
         
         verifiedTokensMockService
-            .given("verified tokens is running")
-            .uponReceiving("a request with a PAN that does not pass Luhn check")
+            .uponReceiving(scenario)
             .withRequest(method: .POST,
                          path: "/verifiedTokens/sessions",
-                         headers: [:],
+                         headers: requestHeaders,
                          body: requestJson)
             .willRespondWith(status: 400,
-                             headers: ["Content-Type": "application/vnd.worldpay.verified-tokens-v1.hal+json;charset=UTF-8"],
+                             headers: responseHeaders,
                              body: responseJson)
         
         let mockDiscovery = MockDiscovery(baseURI: verifiedTokensMockService.baseUrl)
-        let verifiedTokensClient = AccessCheckoutClient(discovery: mockDiscovery, merchantIdentifier: "identity")
+        let verifiedTokensClient = AccessCheckoutClient(discovery: mockDiscovery, merchantIdentifier: request.identity)
         
         verifiedTokensMockService.run(timeout: 10) { testComplete in
-            verifiedTokensClient.createSession(pan: "4111111111111110",
-                                               expiryMonth: 12,
-                                               expiryYear: 2099,
-                                               cvv: "123",
+            verifiedTokensClient.createSession(pan: request.cardNumber,
+                                               expiryMonth: request.expiryMonth,
+                                               expiryYear: request.expiryYear,
+                                               cvv: request.cvc,
                                                urlSession: URLSession.shared) { result in
-                                                switch result {
-                                                case .success(_):
-                                                    XCTFail("Service response expected to be unsuccessful")
-                                                case let .failure(error):
-                                                    XCTAssertTrue(error.localizedDescription.contains("bodyDoesNotMatchSchema"), "Error msg must contain general error code")
-                                                    XCTAssertTrue(error.localizedDescription.contains("panFailedLuhnCheck"), "Error msg must contain specific validation error code")
-                                                    XCTAssertTrue(error.localizedDescription.contains("$.cardNumber"), "Error msg must contain path to error value")
-                                                }
-                                                testComplete()
+                    switch result {
+                    case .success(_):
+                        XCTFail("Service response expected to be unsuccessful")
+                    case let .failure(error):
+                        XCTAssertTrue(error.localizedDescription.contains(response.mainErrorName), "Error msg must contain general error code")
+                        XCTAssertTrue(error.localizedDescription.contains(response.validationErrorName), "Error msg must contain specific validation error code")
+                        XCTAssertTrue(error.localizedDescription.contains(response.validationJsonPath), "Error msg must contain path to error value")
+                    }
+                    testComplete()
             }
         }
     }
