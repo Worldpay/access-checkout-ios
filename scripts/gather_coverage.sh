0b658b9164e0a5b1bf8828d49a827535d2702c74
David Vilares Ferrer <david.vilaresferrer@worldpay.com>
2019-09-25 18:01:56 +0100

    NEB-608: Capture code coverage - no jq

--- scripts/gather_coverage.sh
+++ scripts/gather_coverage.sh
@@ -4,8 +4,10 @@ set -e
 # debug log
 # set -x
 
-export TARGET="${SDK_FRAMEWORK}"
-export COVERAGE=`xcrun xccov view --json --only-targets --report $BITRISE_XCRESULT_PATH | jq --arg TARGET "$TARGET" '.[] | select(.name == $TARGET) | .lineCoverage' | cut -b 3-4`
+export TARGET=${SDK_FRAMEWORK}
+export COVERAGE=`xcrun xccov view --json --only-targets --report $BITRISE_XCRESULT_PATH | grep -E -o lineCoverage[^,]+,[^:]+:\"$TARGET\" | grep -m 1 -o -E '0\.\d{2}' | cut -b 3-4`
 
+echo "*****               *****"
 echo "***** CODE COVERAGE *****"
+echo "*****               *****"
 echo "CODE COVERAGE FOR ${TARGET}: ${COVERAGE}%"
