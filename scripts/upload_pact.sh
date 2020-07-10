e43f3de2d91736b5cf80a45b33f46a1cf581cbdd
Charlie Whight <charlie.whight@worldpay.com>
2020-07-10 15:37:31 +0100

    US349839 add tests to master tagging and pact cli

--- scripts/upload_pact.sh
+++ scripts/upload_pact.sh
@@ -15,10 +15,26 @@ curl --fail --show-error -v -XPUT \-H "Content-Type: application/json" \
   https://$PACTBROKER_URL/pacts/provider/sessions/consumer/access-checkout-iOS-sdk/version/$CONTRACT_VERSION+$HASH_CODE
 
 if [ $BITRISE_GIT_BRANCH == "master" ]
-then
-  curl --fail --show-error -v -XPUT \-H "Content-Type: application/json" \
-  -u $PACTBROKER_USERNAME:$PACTBROKER_PASSWORD \
-  https://$PACTBROKER_URL/pacticipants/access-checkout-iOS-sdk/versions/$CONTRACT_VERSION+$HASH_CODE/tags/$BITRISE_GIT_BRANCH
-else
-  echo "Did not tag as on branch " $BITRISE_GIT_BRANCH
+  then
+    echo "Tagging broker with master..."
+    pact-broker create-version-tag --pacticipant access-checkout-iOS-sdk --version $PROJECT_VERSION+$HASH_CODE --tag $BITRISE_GIT_BRANCH --broker-base-url https://$PACTBROKER_URL -u $PACTBROKER_URL -p $PACTBROKER_PASSWORD
+
+    echo "Verifying tag successful..."
+    DESCRIPTION=$(pact-broker describe-version --pacticipant access-checkout-iOS-sdk --version $PROJECT_VERSION+$HASH_CODE --broker-base-url https://$PACTBROKER_URL -u $PACTBROKER_URL -p $PACTBROKER_PASSWORD)
+    if [[ $DESCRIPTION == *"master"* ]]
+      then
+        echo "Successfully tagged as master"
+      else
+        echo "Failed as broker not successfully tagged"
+        exit 1
+    fi
+  else
+    echo "Verifying not tagged as master..."
+    if [[ $DESCRIPTION == *"master"* ]]
+      then
+        echo "Failed as broker incorrectly tagged with master"
+        exit 1
+      else
+        echo "Did not tag as on branch $BITRISE_GIT_BRANCH"
+    fi
 fi
