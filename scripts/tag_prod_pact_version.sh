e12bd4d18b05ca397e5708a12396772938e9947f
Charlie Whight <charlie.whight@worldpay.com>
2020-07-13 08:37:50 +0100

    US349836 tag pact with prod on deployment

---
+++ scripts/tag_prod_pact_version.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+HASH_CODE=$(git rev-parse --short HEAD)
+
+echo "Tagging with PROD..."
+pact-broker create-version-tag --pacticipant access-checkout-iOS-sdk --version $PROJECT_VERSION+$HASH_CODE --tag PROD --broker-base-url https://$PACTBROKER_URL -u $PACTBROKER_USERNAME -p $PACTBROKER_PASSWORD
+
+echo "Verifying successfully tagged..."
+DESCRIPTION=$(pact-broker describe-version --pacticipant access-checkout-iOS-sdk --version $PROJECT_VERSION+$HASH_CODE --broker-base-url https://$PACTBROKER_URL -u $PACTBROKER_USERNAME -p $PACTBROKER_PASSWORD)
+if [[ $DESCRIPTION == *"PROD"* ]]
+  then
+    echo "Successfully tagged as PROD"
+  else
+    echo "Failed as broker not successfully tagged"
+    exit 1
+fi
