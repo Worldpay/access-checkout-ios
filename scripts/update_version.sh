878d6a45e5796d242f0c3dc58e5872c75c11cbf8
Olivier Chalet <olivier.chalet@worldpay.com>
2021-01-06 16:05:54 +0000

    US438667: modify update version script to change version in pact sh scripts

--- scripts/update_version.sh
+++ scripts/update_version.sh
@@ -8,11 +8,17 @@ updateVersionInPodSpec(){
   sed -i '' '/spec.version/s/[0-9].[0-9].[0-9]/'"$1"'/; /spec.source/s/[0-9].[0-9].[0-9]/'"$1"'/' $2
 }
 
-updateVersionInCode(){
+updateVersionInUserAgent(){
   sed -i '' '/sdkVersion/s/[0-9].[0-9].[0-9]/'"$1"'/' $2
+}
+
+updateVersionInPactScript(){
+  sed -i '' '/CONTRACT_VERSION/s/[0-9].[0-9].[0-9]/'"$1"'/' $2
 }
 
 updateVersionInInfoPlist "$new_version" "../AccessCheckoutSDK/AccessCheckoutSDK/Info.plist"
 updateVersionInInfoPlist "$new_version" "../AccessCheckoutDemo/AccessCheckoutDemo/Info.plist"
 updateVersionInPodSpec "$new_version" "../AccessCheckoutSDK.podspec"
-updateVersionInCode "$new_version" "../AccessCheckoutSDK/AccessCheckoutSDK/api/UserAgent.swift"
+updateVersionInUserAgent "$new_version" "../AccessCheckoutSDK/AccessCheckoutSDK/api/UserAgent.swift"
+updateVersionInPactScript "$new_version" "../scripts/upload_pact.sh"
+updateVersionInPactScript "$new_version" "../scripts/verify_pact_tags.sh"
