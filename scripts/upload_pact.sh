cb7e832a8b8d6a8cbc51128ca90f3ce318382a79
Olivier Chalet <olivier.chalet@worldpay.com>
2021-01-06 16:06:40 +0000

    US438667: change version to 2.1.0 ahead of release

--- scripts/upload_pact.sh
+++ scripts/upload_pact.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-CONTRACT_VERSION="2.0.0"
+CONTRACT_VERSION="2.1.0"
 HASH_CODE=$(git rev-parse --short HEAD)
 
 curl --fail --show-error -v -XPUT \-H "Content-Type: application/json" \
