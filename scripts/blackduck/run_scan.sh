52d38a8a7b9b473576f07f7b729ca8b2f6276dfe
Vishal Jethwa <vishal.jethwa@fisglobal.com>
2021-09-15 11:06:51 +0100

    US637774: fixed variable reference and artifact path

--- scripts/blackduck/run_scan.sh
+++ scripts/blackduck/run_scan.sh
@@ -7,3 +7,4 @@ cd AccessCheckoutSDK
 curl -LOk https://detect.synopsys.com/detect.sh
 chmod +x ./detect.sh
 ./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name=$SDK_VERSION --detect.detector.buildless=true --detect.risk.report.pdf=true
+cd ..
