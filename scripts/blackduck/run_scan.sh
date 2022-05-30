df71aeb626dfbc23389e2cf6dcaf2f94231e60ae
Olivier Chalet <olivier.chalet@worldpay.com>
2022-05-30 12:09:53 +0100

    US1019892: fix issue in Blackduck pipeline where RELEASE SUFFIX is not appended to version name

--- scripts/blackduck/run_scan.sh
+++ scripts/blackduck/run_scan.sh
@@ -6,5 +6,12 @@ SDK_VERSION=$(grep 'spec.version' AccessCheckoutSDK.podspec | cut -d= -f2 | sed 
 cd AccessCheckoutSDK
 curl -LOk https://detect.synopsys.com/detect.sh
 chmod +x ./detect.sh
-./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name=$SDK_VERSION --detect.detector.buildless=true --detect.risk.report.pdf=true
+
+if [ $IS_RELEASE_SCAN -eq 0 ]
+  VERSION_NAME=$SDK_VERSION
+else
+  VERSION_NAME="$SDK_VERSION-RELEASE"
+fi
+
+./detect.sh --blackduck.url="https://fis2.app.blackduck.com/" --blackduck.api.token=$hydra_aco_blackduck_token --blackduck.trust.cert=true --detect.project.name=$BLACKDUCK_PROJECT_NAME --detect.project.version.name=$VERSION_NAME --detect.detector.buildless=true --detect.risk.report.pdf=true
 cd ..
