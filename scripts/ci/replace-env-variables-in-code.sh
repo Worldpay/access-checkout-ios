efd1f4eb1388f8586dd0f024b18bb67b82081741
Olivier Chalet <olivier.chalet@fisglobal.com>
2023-01-11 14:02:58 +0000

    US1372163: change BitRise pipeline to inject env variable using step + upgrade version for compatibility with XCode13 stack in BitRise
     BitRise pipeline now inject env variables using swift-environment-variable-injector step rather than running a script that downloads the binary and executes it.

--- scripts/ci/replace-env-variables-in-code.sh
+++ scripts/ci/replace-env-variables-in-code.sh
@@ -4,8 +4,8 @@ set -e
 # debug log
 set -x
 
-# Download and install Swift environment variable innjector
-curl -ssl https://raw.githubusercontent.com/LucianoPAlmeida/variable-injector/0.3.3/scripts/install-binary.sh | sh
+# Download and install Swift environment variable injector
+curl -ssl https://raw.githubusercontent.com/LucianoPAlmeida/variable-injector/0.3.7/scripts/build-and-install.sh | sh
 
 # Replacing env variables
 variable-injector --file AccessCheckoutDemo/AccessCheckoutDemo/CIVariables.swift --verbose
