8587361cbc8e238c52456f88871ec0a8d659e74d
Olivier Chalet <olivier.chalet@fisglobal.com>
2023-01-12 16:50:54 +0000

    US1376882: add support to override SDK version passed to session services This feature supports only passing access-checkout-react-native/x.y.z or the access-checkout-ios/.a.b.c with a.b.c being the current iOS native SDK version

--- AccessCheckoutSDK/AccessCheckoutSDK/api/UserAgent.swift
+++ AccessCheckoutSDK/AccessCheckoutSDK/api/UserAgent.swift
@@ -1,7 +1,49 @@
-struct UserAgent {
+import Foundation
+
+public struct UserAgent {
     static let sdkVersion = "2.4.0"
     static private let valueFormat = "access-checkout-ios/%@"
     
     static let headerName = "X-WP-SDK"
-    static let headerValue = String(format: valueFormat, UserAgent.sdkVersion)
+    
+    static let defaultHeaderValue = String(format: valueFormat, UserAgent.sdkVersion)
+    private static var _headerValue = defaultHeaderValue
+    static var headerValue:String {
+        get {
+            return _headerValue
+        }
+    }
+    
+    public static func overrideHeaderValue(with value:String) throws {
+        if !validateVersionForOverride(untrustedVersion: value) {
+            throw UserAgentError.invalidVersion()
+        }
+        
+        _headerValue = value
+    }
+    
+    private static func validateVersionForOverride(untrustedVersion:String) -> Bool{
+        if untrustedVersion == defaultHeaderValue {
+            return true
+        }
+        
+        let regExPattern: String = "access\\-checkout\\-react\\-native\\/[0-9]{1,2}\\.[0-9]{1,2}\\.[0-9]{1,2}"
+        let regex = try! NSRegularExpression(pattern: regExPattern)
+        
+        return regex.firstMatch(in: untrustedVersion, range: NSRange(location: 0, length: untrustedVersion.count)) != nil
+    }
+}
+
+struct UserAgentError : Error, Equatable {
+    private static let invalidVersionErrorMessage = "Unsupported version format. This functionality only supports access-checkout-react-native semantic versions or default access-checkout-ios version."
+
+    let message: String
+    
+    private init(message:String) {
+        self.message = message
+    }
+    
+    static func invalidVersion() -> UserAgentError {
+        return UserAgentError(message: invalidVersionErrorMessage)
+    }
 }
