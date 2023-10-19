8b55d5c1a07ac80c6c4edcbd00bf79165acc740c
Olivier Chalet <olivier.chalet@fisglobal.com>
2023-10-19 10:18:24 +0100

    US1721049: add test app to SDK

---
+++ AccessCheckoutSDK/AccessCheckoutSDKTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AccessCheckoutSDKTestApp
+//
+//  Created by Chalet, Olivier on 19/10/2023.
+//  Copyright © 2023 Worldpay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
