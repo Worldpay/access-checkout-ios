37f126e51b8f7052021fa73ead2a18da2747143e
Olivier Chalet <olivier.chalet@fisglobal.com>
2023-10-19 15:58:24 +0100

    US1721049: remove unnecessary comments

--- AccessCheckoutSDK/AccessCheckoutSDKTestApp/SceneDelegate.swift
+++ AccessCheckoutSDK/AccessCheckoutSDKTestApp/SceneDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  SceneDelegate.swift
-//  AccessCheckoutSDKTestApp
-//
-//  Created by Chalet, Olivier on 19/10/2023.
-//  Copyright © 2023 Worldpay. All rights reserved.
-//
-
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
