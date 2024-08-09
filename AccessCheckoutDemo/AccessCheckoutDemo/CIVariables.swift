936c5560218267906b938d1727d4a66a403ce2e8
Olivier Chalet <olivier.chalet@worldpay.com>
2024-08-09 12:19:09 +0100

    US2020332: rename CI variable used for checkout id for clarity

--- AccessCheckoutDemo/AccessCheckoutDemo/CIVariables.swift
+++ AccessCheckoutDemo/AccessCheckoutDemo/CIVariables.swift
@@ -5,7 +5,7 @@ import Foundation
 /// Bitrise Swift variable injector step, see https://github.com/LucianoPAlmeida/variable-injector
 struct CI {
     /// The checkoutId
-    static var checkoutId = "$(MERCHANT_ID)"
+    static var checkoutId = "$(CHECKOUT_ID)"
     
     /// The base URL for Access services
     static var accessBaseUrl = "$(ACCESS_BASE_URL)"
--- AccessCheckoutDemo/AccessCheckoutDemo/Configuration.swift
+++ AccessCheckoutDemo/AccessCheckoutDemo/Configuration.swift
@@ -2,7 +2,7 @@ import Foundation
 import UIKit
 
 struct Configuration {
-    static let checkoutId = CI.checkoutId != "" && CI.checkoutId != "$(MERCHANT_ID)" ? CI.checkoutId : "identity"
+    static let checkoutId = CI.checkoutId != "" && CI.checkoutId != "$(CHECKOUT_ID)" ? CI.checkoutId : "identity"
 
     static var accessBaseUrl: String = ""
 
