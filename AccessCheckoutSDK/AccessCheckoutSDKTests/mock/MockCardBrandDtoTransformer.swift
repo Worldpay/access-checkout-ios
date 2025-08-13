b91576d910f7da940e7c41663a5d0dbd67f5efa6
jmensahWP <jason.dzelamensah@worldpay.com>
2025-08-13 11:38:39 +0100

    US2193424: refactor/ clean up; add mock files; run formatting

--- AccessCheckoutSDK/AccessCheckoutSDKTests/mock/MockCardBrandDtoTransformer.swift
+++ AccessCheckoutSDK/AccessCheckoutSDKTests/mock/MockCardBrandDtoTransformer.swift
@@ -1,94 +1,118 @@
 import Cuckoo
-
 @testable import AccessCheckoutSDK
 
-class MockCardBrandDtoTransformer: CardBrandDtoTransformer, Cuckoo.ClassMock {
 
-    typealias MocksType = CardBrandDtoTransformer
 
-    typealias Stubbing = __StubbingProxy_CardBrandDtoTransformer
-    typealias Verification = __VerificationProxy_CardBrandDtoTransformer
 
-    let cuckoo_manager =
-        Cuckoo.MockManager.preconfiguredManager ?? Cuckoo.MockManager(hasParent: true)
 
+
+ class MockCardBrandDtoTransformer: CardBrandDtoTransformer, Cuckoo.ClassMock {
+    
+     typealias MocksType = CardBrandDtoTransformer
+    
+     typealias Stubbing = __StubbingProxy_CardBrandDtoTransformer
+     typealias Verification = __VerificationProxy_CardBrandDtoTransformer
+
+     let cuckoo_manager = Cuckoo.MockManager.preconfiguredManager ?? Cuckoo.MockManager(hasParent: true)
+
+    
     private var __defaultImplStub: CardBrandDtoTransformer?
 
-    func enableDefaultImplementation(_ stub: CardBrandDtoTransformer) {
+     func enableDefaultImplementation(_ stub: CardBrandDtoTransformer) {
         __defaultImplStub = stub
         cuckoo_manager.enableDefaultStubImplementation()
     }
+    
 
-    override func transform(_ dto: CardBrandDto) -> CardBrandModel {
+    
 
-        return cuckoo_manager.call(
-            """
-            transform(_: CardBrandDto) -> CardBrandModel
-            """,
+    
+
+    
+    
+    
+    
+     override func transform(_ dto: CardBrandDto) -> CardBrandModel {
+        
+    return cuckoo_manager.call(
+    """
+    transform(_: CardBrandDto) -> CardBrandModel
+    """,
             parameters: (dto),
             escapingParameters: (dto),
             superclassCall:
-
-                super.transform(dto),
+                
+                super.transform(dto)
+                ,
             defaultCall: __defaultImplStub!.transform(dto))
-
+        
     }
+    
+    
 
-    struct __StubbingProxy_CardBrandDtoTransformer: Cuckoo.StubbingProxy {
+     struct __StubbingProxy_CardBrandDtoTransformer: Cuckoo.StubbingProxy {
         private let cuckoo_manager: Cuckoo.MockManager
-
-        init(manager: Cuckoo.MockManager) {
+    
+         init(manager: Cuckoo.MockManager) {
             self.cuckoo_manager = manager
         }
-
-        func transform<M1: Cuckoo.Matchable>(_ dto: M1)
-            -> Cuckoo.ClassStubFunction<(CardBrandDto), CardBrandModel>
-        where M1.MatchedType == CardBrandDto {
+        
+        
+        
+        
+        func transform<M1: Cuckoo.Matchable>(_ dto: M1) -> Cuckoo.ClassStubFunction<(CardBrandDto), CardBrandModel> where M1.MatchedType == CardBrandDto {
             let matchers: [Cuckoo.ParameterMatcher<(CardBrandDto)>] = [wrap(matchable: dto) { $0 }]
-            return .init(
-                stub: cuckoo_manager.createStub(
-                    for: MockCardBrandDtoTransformer.self,
-                    method:
-                        """
-                        transform(_: CardBrandDto) -> CardBrandModel
-                        """, parameterMatchers: matchers))
+            return .init(stub: cuckoo_manager.createStub(for: MockCardBrandDtoTransformer.self, method:
+    """
+    transform(_: CardBrandDto) -> CardBrandModel
+    """, parameterMatchers: matchers))
         }
-
+        
+        
     }
 
-    struct __VerificationProxy_CardBrandDtoTransformer: Cuckoo.VerificationProxy {
+     struct __VerificationProxy_CardBrandDtoTransformer: Cuckoo.VerificationProxy {
         private let cuckoo_manager: Cuckoo.MockManager
         private let callMatcher: Cuckoo.CallMatcher
         private let sourceLocation: Cuckoo.SourceLocation
-
-        init(
-            manager: Cuckoo.MockManager, callMatcher: Cuckoo.CallMatcher,
-            sourceLocation: Cuckoo.SourceLocation
-        ) {
+    
+         init(manager: Cuckoo.MockManager, callMatcher: Cuckoo.CallMatcher, sourceLocation: Cuckoo.SourceLocation) {
             self.cuckoo_manager = manager
             self.callMatcher = callMatcher
             self.sourceLocation = sourceLocation
         }
-
+    
+        
+    
+        
+        
+        
         @discardableResult
-        func transform<M1: Cuckoo.Matchable>(_ dto: M1)
-            -> Cuckoo.__DoNotUse<(CardBrandDto), CardBrandModel>
-        where M1.MatchedType == CardBrandDto {
+        func transform<M1: Cuckoo.Matchable>(_ dto: M1) -> Cuckoo.__DoNotUse<(CardBrandDto), CardBrandModel> where M1.MatchedType == CardBrandDto {
             let matchers: [Cuckoo.ParameterMatcher<(CardBrandDto)>] = [wrap(matchable: dto) { $0 }]
             return cuckoo_manager.verify(
-                """
-                transform(_: CardBrandDto) -> CardBrandModel
-                """, callMatcher: callMatcher, parameterMatchers: matchers,
-                sourceLocation: sourceLocation)
+    """
+    transform(_: CardBrandDto) -> CardBrandModel
+    """, callMatcher: callMatcher, parameterMatchers: matchers, sourceLocation: sourceLocation)
         }
-
+        
+        
     }
 }
 
-class CardBrandDtoTransformerStub: CardBrandDtoTransformer {
 
-    override func transform(_ dto: CardBrandDto) -> CardBrandModel {
+ class CardBrandDtoTransformerStub: CardBrandDtoTransformer {
+    
+
+    
+
+    
+    
+    
+    
+     override func transform(_ dto: CardBrandDto) -> CardBrandModel  {
         return DefaultValueRegistry.defaultValue(for: (CardBrandModel).self)
     }
-
+    
+    
 }
--- AccessCheckoutSDK/AccessCheckoutSDKTests/mock/MockExpiryDateValidator.swift
+++ AccessCheckoutSDK/AccessCheckoutSDKTests/mock/MockExpiryDateValidator.swift
@@ -1,137 +1,171 @@
 import Cuckoo
-import Foundation
-
 @testable import AccessCheckoutSDK
 
-class MockExpiryDateValidator: ExpiryDateValidator, Cuckoo.ClassMock {
+import Foundation
 
-    typealias MocksType = ExpiryDateValidator
 
-    typealias Stubbing = __StubbingProxy_ExpiryDateValidator
-    typealias Verification = __VerificationProxy_ExpiryDateValidator
 
-    let cuckoo_manager =
-        Cuckoo.MockManager.preconfiguredManager ?? Cuckoo.MockManager(hasParent: true)
 
+
+
+ class MockExpiryDateValidator: ExpiryDateValidator, Cuckoo.ClassMock {
+    
+     typealias MocksType = ExpiryDateValidator
+    
+     typealias Stubbing = __StubbingProxy_ExpiryDateValidator
+     typealias Verification = __VerificationProxy_ExpiryDateValidator
+
+     let cuckoo_manager = Cuckoo.MockManager.preconfiguredManager ?? Cuckoo.MockManager(hasParent: true)
+
+    
     private var __defaultImplStub: ExpiryDateValidator?
 
-    func enableDefaultImplementation(_ stub: ExpiryDateValidator) {
+     func enableDefaultImplementation(_ stub: ExpiryDateValidator) {
         __defaultImplStub = stub
         cuckoo_manager.enableDefaultStubImplementation()
     }
+    
 
-    override func validate(_ expiryDate: String) -> Bool {
+    
 
-        return cuckoo_manager.call(
-            """
-            validate(_: String) -> Bool
-            """,
+    
+
+    
+    
+    
+    
+     override func validate(_ expiryDate: String) -> Bool {
+        
+    return cuckoo_manager.call(
+    """
+    validate(_: String) -> Bool
+    """,
             parameters: (expiryDate),
             escapingParameters: (expiryDate),
             superclassCall:
-
-                super.validate(expiryDate),
+                
+                super.validate(expiryDate)
+                ,
             defaultCall: __defaultImplStub!.validate(expiryDate))
-
+        
     }
-
-    override func canValidate(_ text: String) -> Bool {
-
-        return cuckoo_manager.call(
-            """
-            canValidate(_: String) -> Bool
-            """,
+    
+    
+    
+    
+    
+     override func canValidate(_ text: String) -> Bool {
+        
+    return cuckoo_manager.call(
+    """
+    canValidate(_: String) -> Bool
+    """,
             parameters: (text),
             escapingParameters: (text),
             superclassCall:
-
-                super.canValidate(text),
+                
+                super.canValidate(text)
+                ,
             defaultCall: __defaultImplStub!.canValidate(text))
-
+        
     }
+    
+    
 
-    struct __StubbingProxy_ExpiryDateValidator: Cuckoo.StubbingProxy {
+     struct __StubbingProxy_ExpiryDateValidator: Cuckoo.StubbingProxy {
         private let cuckoo_manager: Cuckoo.MockManager
-
-        init(manager: Cuckoo.MockManager) {
+    
+         init(manager: Cuckoo.MockManager) {
             self.cuckoo_manager = manager
         }
-
-        func validate<M1: Cuckoo.Matchable>(_ expiryDate: M1)
-            -> Cuckoo.ClassStubFunction<(String), Bool> where M1.MatchedType == String
-        {
+        
+        
+        
+        
+        func validate<M1: Cuckoo.Matchable>(_ expiryDate: M1) -> Cuckoo.ClassStubFunction<(String), Bool> where M1.MatchedType == String {
             let matchers: [Cuckoo.ParameterMatcher<(String)>] = [wrap(matchable: expiryDate) { $0 }]
-            return .init(
-                stub: cuckoo_manager.createStub(
-                    for: MockExpiryDateValidator.self,
-                    method:
-                        """
-                        validate(_: String) -> Bool
-                        """, parameterMatchers: matchers))
+            return .init(stub: cuckoo_manager.createStub(for: MockExpiryDateValidator.self, method:
+    """
+    validate(_: String) -> Bool
+    """, parameterMatchers: matchers))
         }
-
-        func canValidate<M1: Cuckoo.Matchable>(_ text: M1)
-            -> Cuckoo.ClassStubFunction<(String), Bool> where M1.MatchedType == String
-        {
+        
+        
+        
+        
+        func canValidate<M1: Cuckoo.Matchable>(_ text: M1) -> Cuckoo.ClassStubFunction<(String), Bool> where M1.MatchedType == String {
             let matchers: [Cuckoo.ParameterMatcher<(String)>] = [wrap(matchable: text) { $0 }]
-            return .init(
-                stub: cuckoo_manager.createStub(
-                    for: MockExpiryDateValidator.self,
-                    method:
-                        """
-                        canValidate(_: String) -> Bool
-                        """, parameterMatchers: matchers))
+            return .init(stub: cuckoo_manager.createStub(for: MockExpiryDateValidator.self, method:
+    """
+    canValidate(_: String) -> Bool
+    """, parameterMatchers: matchers))
         }
-
+        
+        
     }
 
-    struct __VerificationProxy_ExpiryDateValidator: Cuckoo.VerificationProxy {
+     struct __VerificationProxy_ExpiryDateValidator: Cuckoo.VerificationProxy {
         private let cuckoo_manager: Cuckoo.MockManager
         private let callMatcher: Cuckoo.CallMatcher
         private let sourceLocation: Cuckoo.SourceLocation
-
-        init(
-            manager: Cuckoo.MockManager, callMatcher: Cuckoo.CallMatcher,
-            sourceLocation: Cuckoo.SourceLocation
-        ) {
+    
+         init(manager: Cuckoo.MockManager, callMatcher: Cuckoo.CallMatcher, sourceLocation: Cuckoo.SourceLocation) {
             self.cuckoo_manager = manager
             self.callMatcher = callMatcher
             self.sourceLocation = sourceLocation
         }
-
+    
+        
+    
+        
+        
+        
         @discardableResult
-        func validate<M1: Cuckoo.Matchable>(_ expiryDate: M1) -> Cuckoo.__DoNotUse<(String), Bool>
-        where M1.MatchedType == String {
+        func validate<M1: Cuckoo.Matchable>(_ expiryDate: M1) -> Cuckoo.__DoNotUse<(String), Bool> where M1.MatchedType == String {
             let matchers: [Cuckoo.ParameterMatcher<(String)>] = [wrap(matchable: expiryDate) { $0 }]
             return cuckoo_manager.verify(
-                """
-                validate(_: String) -> Bool
-                """, callMatcher: callMatcher, parameterMatchers: matchers,
-                sourceLocation: sourceLocation)
+    """
+    validate(_: String) -> Bool
+    """, callMatcher: callMatcher, parameterMatchers: matchers, sourceLocation: sourceLocation)
         }
-
+        
+        
+        
+        
         @discardableResult
-        func canValidate<M1: Cuckoo.Matchable>(_ text: M1) -> Cuckoo.__DoNotUse<(String), Bool>
-        where M1.MatchedType == String {
+        func canValidate<M1: Cuckoo.Matchable>(_ text: M1) -> Cuckoo.__DoNotUse<(String), Bool> where M1.MatchedType == String {
             let matchers: [Cuckoo.ParameterMatcher<(String)>] = [wrap(matchable: text) { $0 }]
             return cuckoo_manager.verify(
-                """
-                canValidate(_: String) -> Bool
-                """, callMatcher: callMatcher, parameterMatchers: matchers,
-                sourceLocation: sourceLocation)
+    """
+    canValidate(_: String) -> Bool
+    """, callMatcher: callMatcher, parameterMatchers: matchers, sourceLocation: sourceLocation)
         }
-
+        
+        
     }
 }
 
-class ExpiryDateValidatorStub: ExpiryDateValidator {
 
-    override func validate(_ expiryDate: String) -> Bool {
+ class ExpiryDateValidatorStub: ExpiryDateValidator {
+    
+
+    
+
+    
+    
+    
+    
+     override func validate(_ expiryDate: String) -> Bool  {
         return DefaultValueRegistry.defaultValue(for: (Bool).self)
     }
-
-    override func canValidate(_ text: String) -> Bool {
+    
+    
+    
+    
+    
+     override func canValidate(_ text: String) -> Bool  {
         return DefaultValueRegistry.defaultValue(for: (Bool).self)
     }
-
+    
+    
 }
