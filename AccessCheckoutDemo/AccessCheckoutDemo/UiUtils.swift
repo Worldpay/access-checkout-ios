ebe4ab7a208621fca1f862164d9380c48e84cd47
Olivier Chalet <olivier.chalet@worldpay.com>
2025-09-16 00:53:57 +0100

    US2126883: implement caching mechanism in Demo app to speed up loading of card brand images

--- AccessCheckoutDemo/AccessCheckoutDemo/UiUtils.swift
+++ AccessCheckoutDemo/AccessCheckoutDemo/UiUtils.swift
@@ -3,29 +3,50 @@ import Foundation
 import UIKit
 
 final class UiUtils {
-    private static let unknownBrandImage = UIImage(named: "card_unknown")
+    private static let unknownBrandImage = UIImage(named: "card_unknown")!
+    private static var cardBrandsImagesCache : [String: CachedImage] = [:]
 
     public static func updateCardBrandImage(_ imageView: UIImageView, with cardBrand: CardBrand?) {
         if let cardBrand = cardBrand,
-            let imageUrl = cardBrand.images.first(where: { $0.type == "image/png" })?.url,
-            let url = URL(string: imageUrl)
+            let urlAsString = cardBrand.images.first(where: { $0.type == "image/png" })?.url,
+            let url = URL(string: urlAsString)
         {
-            // User interactive QOS is chosen to make the task below a high priority task
-            // This contributes strongly to seeing updates to the UI made faster
-            let serialQueue = DispatchQueue(label: "worldpay.demo.uiutils", qos: .userInteractive)
-            serialQueue.async {
-                if let data = try? Data(contentsOf: url) {
-                    DispatchQueue.main.async {
-                        imageView.image = UIImage(data: data)
-                        imageView.accessibilityLabel = NSLocalizedString(cardBrand.name, comment: "")
+            if let cachedImage = cardBrandsImagesCache[urlAsString] {
+                setImageSourceAndLabel(imageView, source: UIImage(data: cachedImage.uiImage)!, label: cachedImage.label)
+            } else {
+                // User interactive QOS is chosen to make the task below a high priority task
+                // This contributes strongly to seeing updates to the UI made faster
+                let serialQueue = DispatchQueue(label: "worldpay.demo.uiutils", qos: .userInteractive)
+                serialQueue.async {
+                    if let data = try? Data(contentsOf: url) {
+                        let cachedImage = CachedImage(uiImage: data, label: cardBrand.name)
+                        cardBrandsImagesCache[urlAsString] = cachedImage
+                        
+                        DispatchQueue.main.async {
+                            setImageSourceAndLabel(imageView, source: UIImage(data: cachedImage.uiImage)!, label: cachedImage.label)
+                        }
                     }
                 }
             }
         } else {
             DispatchQueue.main.async {
+                setImageSourceAndLabel(imageView, source: unknownBrandImage, label: "unknown_card_brand")
+                
                 imageView.image = unknownBrandImage
                 imageView.accessibilityLabel = NSLocalizedString("unknown_card_brand", comment: "")
             }
         }
+    }
+    
+    private static func setImageSourceAndLabel(_ imageView: UIImageView, source: UIImage, label: String) {
+        DispatchQueue.main.async {
+            imageView.image = source
+            imageView.accessibilityLabel = NSLocalizedString(label, comment: "")
+        }
+    }
+    
+    private struct CachedImage {
+        fileprivate let uiImage:Data
+        fileprivate let label:String
     }
 }
