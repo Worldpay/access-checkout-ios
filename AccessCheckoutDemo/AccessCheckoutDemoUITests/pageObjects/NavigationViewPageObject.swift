27ec3322ad719cef667217a14cc64156f4d7f1a1
TyreseWp <tyrese.thomas@fisglobal.com>
2025-11-14 12:41:19 +0000

    NOTICKET: tab bar navigation refactored to resolve iPad issues

--- AccessCheckoutDemo/AccessCheckoutDemoUITests/pageObjects/NavigationViewPageObject.swift
+++ AccessCheckoutDemo/AccessCheckoutDemoUITests/pageObjects/NavigationViewPageObject.swift
@@ -2,17 +2,20 @@ import XCTest
 
 class NavigationViewPageObject {
     private let app: XCUIApplication
+    
+    private let tabBarIdentifier = "tabBar"
+    private let tabBarItemIdentifier = "tabBarItem"
 
     var cvcFlowNavigationButton: XCUIElement {
-        return app.tabBars.buttons["CVC Flow"]
+        return app.buttons["\(tabBarItemIdentifier)_2"].firstMatch
     }
 
     var cardFlowNavigationButton: XCUIElement {
-        return app.tabBars.buttons["Card Flow"]
+        return app.buttons["\(tabBarItemIdentifier)_0"].firstMatch
     }
 
     var restrictedCardFlowNavigationButton: XCUIElement {
-        return app.tabBars.buttons["Restricted Card Flow"]
+        return app.buttons["\(tabBarItemIdentifier)_1"].firstMatch
     }
 
     init(_ app: XCUIApplication) {
