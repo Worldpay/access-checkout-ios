1e5a777c0ff8cd72a26fa267f9aa7e26d846a4d0
Alejandro Baeza Gutierrez <alejandro.baezagutierrez@worldpay.com>
2026-01-09 13:49:00 +0000

    fix: Resolve issue where default iOS calendar for the Thai region is the Buddhist calendar instead of the Gregorian calendar, this causes an issue where expiry dates could not be validated since the current date produced in devices with Buddhist calendar would show as 543 years in the future, issue is addressed by pinning the calendar instance to gregorian calendar

---
+++ AccessCheckoutDemo/AccessCheckoutDemoUITests/CardFlowSystemCalendarValidationTests.swift
@@ -0,0 +1,77 @@
+import XCTest
+
+class CardFlowSystemCalendarValidationTests: BaseUITest {
+    private let backspace = String(XCUIKeyboardKey.delete.rawValue)
+    
+    var view: CardFlowViewPageObject?
+
+    override func setUp() {
+        super.setUp()
+        view = CardFlowViewPageObject(app)
+    }
+   
+    // MARK: Expiry Date validation
+
+    func testExpiryDate_doesNotAccepteAlphabeticalCharacters() {
+        view!.typeTextIntoExpiryDate("A")
+
+        XCTAssertEqual(view!.expiryDateField.placeholderValue, view!.expiryDateText)
+    }
+
+    func testExpiryDate_canDeleteDigits() {
+        view!.typeTextIntoExpiryDate("12/99")
+        XCTAssertEqual(view!.expiryDateText, "12/99")
+
+        view!.typeTextIntoExpiryDate(backspace)
+        XCTAssertEqual(view!.expiryDateText, "12/9")
+    }
+
+    func testExpiryDate_isInvalidAfterAllTextIsCleared() {
+        view!.typeTextIntoExpiryDate("12/99")
+        XCTAssertEqual(view!.expiryDateIsValidLabel.label, "valid")
+
+        view!.clearField(view!.expiryDateField)
+
+        XCTAssertEqual(view!.expiryDateField.placeholderValue, view!.expiryDateText)
+        XCTAssertEqual(view!.expiryDateIsValidLabel.label, "invalid")
+    }
+
+    func testExpiryDate_acceptsUpTo5Characters() {
+        view!.typeTextIntoExpiryDate("12/999")
+
+        XCTAssertEqual(view!.expiryDateText!, "12/99")
+        XCTAssertEqual(view!.expiryDateText!.count, 5)
+    }
+
+    func testPartialExpiryDateIsInvalid() {
+        view!.typeTextIntoExpiryDate("12")
+
+        view!.panField.tap()  // we move the focus to another field so that the validation triggers
+
+        XCTAssertEqual(view!.expiryDateIsValidLabel.label, "invalid")
+    }
+
+    func testCompleteExpiryDateIsValid() {
+        view!.typeTextIntoExpiryDate("12/99")
+
+        XCTAssertEqual(view!.expiryDateIsValidLabel.label, "valid")
+    }
+
+
+    // MARK: Submit button
+    func testSubmit_isEnabledWhenCardDetailsAreValid() {
+        XCTAssertFalse(view!.submitButton.isEnabled)
+
+        view!.typeTextIntoPanCharByChar("4111111111111111")
+        view!.typeTextIntoExpiryDate("01/34")
+        view!.typeTextIntoCvc("123")
+
+        // Valid state
+        XCTAssertTrue(view!.submitButton.isEnabled)
+
+        view!.typeTextIntoCvc(backspace)
+
+        // Invalid state
+        XCTAssertFalse(view!.submitButton.isEnabled)
+    }
+}
